d37ce616005d748c4de92cb82d722531dad341cc
selvaganapathi.a <kdselva57@gmail.com>
2022-10-10 17:50:46 +0530

    new

---
+++ index.js
@@ -0,0 +1,8 @@
+function intro() {
+	document.getElementById("main").style.display = "block";
+	document.getElementById("myFrame").src = "DS/intro.html";
+  }
+  function author(){
+	document.getElementById("main").style.display = "block";
+	document.getElementById("myFrame").src = "https://selvacv.netlify.app/";
+  }
