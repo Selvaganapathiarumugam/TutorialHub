b071ce2f8fd9dc1f3a6c7eaf6f58d70e29fd3039
selvaganapathi.a <kdselva57@gmail.com>
2022-10-12 18:01:55 +0530

    link

--- index.js
+++
@@ -1,8 +0,0 @@
-function intro() {
-	document.getElementById("main").style.display = "block";
-	document.getElementById("myFrame").src = "DS/intro.html";
-  }
-  function Stack() {
-	document.getElementById("main").style.display = "block";
-	document.getElementById("myFrame").src = "DS/Stack.html";
-  }
