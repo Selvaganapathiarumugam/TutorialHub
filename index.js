1779155771527ad0a10e0ea90f2080bc5d0e341d
selvaganapathi.a <kdselva57@gmail.com>
2022-10-12 17:38:25 +0530

    Stack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function intro() {
 	document.getElementById("main").style.display = "block";
 	document.getElementById("myFrame").src = "DS/intro.html";
   }
-  function author(){
+  function Stack() {
 	document.getElementById("main").style.display = "block";
-	document.getElementById("myFrame").src = "https://selvacv.netlify.app/";
+	document.getElementById("myFrame").src = "DS/Stack.html";
   }
