7ba0641087764fe3bbe922e43ab01ddec97ad0ec
selvaganapathi.a <kdselva57@gmail.com>
2022-10-14 19:14:03 +0530

    route

--- Learning_portal/src/app/app-routing.module.ts
+++ Learning_portal/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HtmlComponent} from './html/html.component'
+const routes: Routes = [
+  {path:'html',component:HtmlComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Learning_portal/src/app/app.component.ts
+++ Learning_portal/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router  } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Learning_portal';
+  constructor(private router:Router){}
+
+
+  goToPage(pageName:string):void{
+    this.router.navigate([`${pageName}`])
+  }
 }
